fd79c9b48eb1df3f6ba149636a67c530f83495d7
ntmonov <ntm@mail.bg>
2018-08-24 16:04:56 +0300

    Interceptor removed

--- src/app/core/services/cars.service.ts
+++ src/app/core/services/cars.service.ts
@@ -16,44 +16,66 @@ export class CarsService {
 
   constructor(private http: HttpClient) { }
 
+  private cteateAuthHeaders(type: string) : HttpHeaders {
+    if (type == 'Basic') {
+      return new HttpHeaders({
+                'Authorization': `Basic ${btoa(`${appKey}:${appSecret}`)}`,
+                'Content-Type': 'application/json'
+      })
+    } else {
+      return new HttpHeaders({
+            'Authorization': `Kinvey ${sessionStorage.getItem('authtoken')}`,
+            'Content-Type': 'application/json'
+      })
+    }
+  }
+
   create(car: CarModel): Observable<CarModel>{
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}`
-    return this.http.post<CarModel>(url, JSON.stringify(car))
+    return this.http.post<CarModel>(url, JSON.stringify(car), { headers })
   }
 
   list(): Observable<Array<CarModel>> {
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}`
-    return this.http.get<Array<CarModel>>(url)
+    return this.http.get<Array<CarModel>>(url, { headers })
   }
 
   details(id): Observable<CarModel> {
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}/${id}`
-    return this.http.get<CarModel>(url)  
+    return this.http.get<CarModel>(url, { headers })  
   }
 
   getById(id) : Observable<CarModel> {
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}/${id}`
-    return this.http.get<CarModel>(url)  
+    return this.http.get<CarModel>(url, { headers })  
   }
 
   edit(car: CarModel) : Observable<CarModel> {
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}/${car['_id']}`
-    return this.http.put<CarModel>(url, JSON.stringify(car))
+    return this.http.put<CarModel>(url, JSON.stringify(car), { headers })
   }
 
   delete(id) {
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}/${id}`
-    return this.http.delete(url)
+    return this.http.delete(url, { headers })
   }
 
   getAuthor(carId: string): Observable<Object> {
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}/${carId}`
-    return this.http.get<Observable<Object>>(url)
+    return this.http.get<Observable<Object>>(url, { headers })
   }
 
   getMyCars(userId: string): Observable<Array<CarModel>> {
+    let headers = this.cteateAuthHeaders('Kinvey')
     let url = `${baseUrl}${module}/${appKey}/${collection}/?query={"_acl.creator":"${userId}"}`
-    return this.http.get<Array<CarModel>>(url)
+    return this.http.get<Array<CarModel>>(url, { headers })
   }
 }
 
